d57dc0ccfcbc719b693fcaefa7b6d84f95961063
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 17:41:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -40,7 +40,7 @@ def return_string_array(array)
   counter = 0
   new_array = []
   while counter < array.length do
-    new_array << array[count].to_s
+    new_array << array[counter].to_s
     counter += 1
   end
   new_array
