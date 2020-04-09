ef5020c1869657206ad0c1e6baf0a6fdea29d05c
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 17:02:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,12 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  counter = 0
+  
+  while counter <= 5 do
+    puts message[counter]
+  end
+end
+
+message = "Hello World."
+loop_message_five_times(message)
