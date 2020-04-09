08960d9b5c10ad069d50712e64bc0254e4916fe0
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 17:19:36 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,15 +14,13 @@ loop_message_five_times(message)
 
 ##########################################################
 
-def loop_message_n_times(message1, number1)
+def loop_message_n_times(message, limit)
   counter = 0
   
-  while counter <= 5 do
-    puts "#{message1} to be output #{number1} times"
+  while counter < limit do
+    puts message
     counter += 1
   end
 end
 
-message1 = "Hello moon."
-number1 = 5
-loop_message_n_times(message1, number1)
+
